c767b5baa2e8a548c9097635736ae947b6aa5deb
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-06 16:42:20 -0500

    Full CRUD on Posts Model

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,36 @@
 class Api::PostsController < ApplicationController
+
+    def index
+        # /api/users/:user_id/posts(.:format)
+        @posts = User.find(params[:user_id]).post
+        render json: @posts
+    end
+
+    def show
+        @user = User.find(params[:user_id])
+        @post = @user.post.find(params[:id])
+        render json: @post
+    end
+
+    def create
+        @user = User.find(params[:user_id])
+        @post = @user.post.create(post_params)
+        render json: @post
+    end
+
+    def update
+        @user = User.find(params[:user_id])
+        @post = @user.post.find(params[:id]).update(post_params)
+        render json: @post
+    end
+
+    def destroy
+        @user = User.find(params[:user_id])
+        @post = @user.post.find(params[:id]).delete
+        render json: @post
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :content, :bird_id)
+    end
 end
