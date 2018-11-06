b22e6357a373229aeb9b3c74ae0082df92221675
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-06 15:12:04 -0500

    FULL CRUD on Users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,34 @@
 class Api::UsersController < ApplicationController
+
+def index
+    @users = User.all
+    render json: @users
+end
+
+def show
+    @user = User.find(params[:id])
+    render json: @user
+end
+
+def create
+    @user = User.create(user_params)
+    render json: @user
+end
+
+def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+end
+
+def destroy
+    @user = User.find(params[:id]).delete
+    render status: 200
+end
+
+def user_params
+    params.require(:user).permit(:username, :emailaddress)
+end
+
+
 end
