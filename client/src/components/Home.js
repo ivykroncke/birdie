8e9f2b9c046801bbc68e9af5e0370ebe90893502
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-06 19:55:43 -0500

    App wrapper with styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+@import url('https://fonts.googleapis.com/css?family=Lora|Roboto');
+font-family: Roboto, sans-serif;
+a {
+  color: inherit;
+  text-decoration: none;
+}
+`
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
+      <AppWrapper>
         <div>HOME</div>
         <Link to="/login">Login</Link>
-      </div>
+      </AppWrapper>
     )
   }
 }
