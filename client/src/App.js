1cf14225b8c6adbc5f7938e1a7caec966e21f83f
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-06 20:07:48 -0500

    Quick styling on home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home';
 import Login from './components/Login';
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+@import url('https://fonts.googleapis.com/css?family=Lora|Roboto');
+font-family: Roboto, sans-serif;
+a {
+  color: inherit;
+  text-decoration: none;
+}
+h1 {
+  font-family: Lora, sans-serif;
+}
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppWrapper>
         <Router>
           <Switch>
             <Route exact path="/" component={Home}/>
             <Route exact path="/login" component={Login} />
           </Switch>
         </Router>
-      </div>
+      </AppWrapper>
     )
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-const AppWrapper = styled.div`
-@import url('https://fonts.googleapis.com/css?family=Lora|Roboto');
-font-family: Roboto, sans-serif;
-a {
-  color: inherit;
-  text-decoration: none;
-}
+const HomeWrapper = styled.div`
+height: 100vh;
+display: flex;
+justify-content: space-around;
+align-items: center;
+flex-direction: column;
+background-image: url('https://content.presspage.com/uploads/1763/1920_year-of-the-bird-george-grall.jpg?10000');
+background-size: cover;
+`
+
+const AppName = styled.h1`
+font-size: 4rem;
+`
+
+const StyledLogin = styled(Link)`
+padding: .5rem;
+border: .1rem solid black;
+border-radius: 15%;
 `
 
 export default class Home extends Component {
   render() {
     return (
-      <AppWrapper>
-        <div>HOME</div>
-        <Link to="/login">Login</Link>
-      </AppWrapper>
+      <HomeWrapper>
+        <AppName>birdie</AppName>
+        <StyledLogin to="/login">Login</StyledLogin>
+      </HomeWrapper>
     )
   }
 }
