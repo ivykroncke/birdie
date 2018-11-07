6983c629e5847f36e5e1b1ef25810088e189ddaa
Ivy Kroncke <ivy.lane.kroncke@gmail.com>
2018-11-06 22:15:28 -0500

    Create User shows static content in state and Create user adds a user to the database

--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+
+const AddUserContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+align-items: center;
+`
+
+const StyledInput = styled.input`
+border: .25rem darkgray solid;
+padding: .5rem;
+margin: 3rem;
+width: 50%;
+`
+
+const StyledSubmit = styled.div`
+background-color: darkgray;
+color: white;
+border-radius: 15%;
+padding: 1rem;
+`
 
 export default class CreateUser extends Component {
+  state = {
+    newUser: {
+      username: '',
+      emailaddress: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const newUser = { ...this.state.newUser }
+    newUser[event.target.name] = event.target.value
+    this.setState({ newUser })
+  }
+
+  addUser = async (event) => {
+    event.preventDefault()
+    await axios.post('/api/users', this.state.newUser)
+  }
+
+
   render() {
     return (
-      <div>
-        Create User
-      </div>
+      <AddUserContainer>
+        <form>
+            <div>Username</div>
+            <StyledInput type='text' name='username' 
+              onChange={this.handleChange} />
+            <div>E-mail Address</div>
+            <StyledInput type='text' name='emailaddress' 
+              onChange={this.handleChange} />
+            <StyledSubmit type="submit" onClick={this.addUser}>Submit</StyledSubmit>
+        </form>
+      </AddUserContainer>
     )
   }
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import CreateUser from './CreateUser'
+import axios from 'axios'
+
+const LoginContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+margin: 0 2rem;
+`
+
+const StyledLogin = styled.h1`
+padding: 2rem;
+`
+
+const StyledUsers = styled.div`
+text-align: center;
+`
+
+const StyledUser = styled.div`
+padding: 1rem;
+`
+
+const ButtonForCreateNewUser = styled.div`
+padding: .5rem;
+border: .1rem solid black;
+border-radius: 15%;
+margin: 2rem;
+`
 
 export default class Login extends Component {
+  state = {
+    users : [
+      {username: "Ivy"},
+      {username: "Fritz"},
+      {username: "Charlotte"}
+    ],
+    createNewUserToggle: true
+  }
+
+  toggleCreateNewUser = () => {
+    this.setState({ createNewUserToggle: !this.state.createNewUserToggle })
+  }
+
   render() {
+
+    const UsersList = this.state.users.map((user, i) => {
+      return (
+      <StyledUser key={i}>
+        <div>{user.username} </div> 
+      </StyledUser>
+      )
+    })
+
     return (
-      <div>
-        <div>LOGIN</div>
-        <div>List of Logins to choose from</div>
-        <div>this will be a ternary swapping to Create User</div>
-        <div>this will be a ternary swapping to Update/Delete User</div>
-      </div>
+      <LoginContainer>
+        <StyledLogin>sign in</StyledLogin>
+        {this.state.createNewUserToggle 
+           ?(<div>
+              <StyledUsers>{UsersList}</StyledUsers>
+              <ButtonForCreateNewUser onClick={this.toggleCreateNewUser}>
+                Create a New User
+              </ButtonForCreateNewUser>
+           </div>) :
+            (<CreateUser />)}
+      </LoginContainer>
     )
   }
 }
